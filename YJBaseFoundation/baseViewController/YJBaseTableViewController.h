14784eb6e8ecd5f20ee03792165a140ac61d8d26
htkj <137932814@qq.com>
2018-09-05 09:37:00 +0800

    updateFundation

---
+++ YJBaseFoundation/baseViewController/YJBaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJBaseTableViewController.h
+//  Demo
+//
+//  Created by xhkj on 2018/9/4.
+//  Copyright © 2018年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJBaseTableViewController : UITableViewController
+
+@end

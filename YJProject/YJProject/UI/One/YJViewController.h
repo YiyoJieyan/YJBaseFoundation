5eb4b490d94d56d525db6597dfe909136acc1569
yiyojieyan <yiyojieyan@126.com>
2018-09-11 17:17:51 +0800

    2018年09月11日17:17:49

---
+++ YJProject/YJProject/UI/One/YJViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJViewController.h
+//  YJProject
+//
+//  Created by 邓钦武 on 2018/9/11.
+//  Copyright © 2018年 YJ. All rights reserved.
+//
+
+#import "YJBaseViewController.h"
+
+@interface YJViewController : YJBaseViewController
+
+@end

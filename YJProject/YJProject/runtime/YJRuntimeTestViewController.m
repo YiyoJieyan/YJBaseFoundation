92c12bceb817053766a258f4037627a3c391b297
htkj <137932814@qq.com>
2018-10-22 10:37:37 +0800

    sj

---
+++ YJProject/YJProject/runtime/YJRuntimeTestViewController.m
@@ -0,0 +1,32 @@
+//
+//  YJRuntimeTestViewController.m
+//  YJProject
+//
+//  Created by xhkj on 2018/10/10.
+//  Copyright © 2018年 YJ. All rights reserved.
+//
+
+#import "YJRuntimeTestViewController.h"
+
+@interface YJRuntimeTestViewController ()
+
+@end
+
+@implementation YJRuntimeTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
